357e68aa80de2cdcc6e1a5938568fcfaaf6df47f
Gino Ascencio <gino.francisco@hotmail.com>
2022-11-13 11:50:08 -0500

    add form builder

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -9,11 +9,17 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class BasicsComponent implements OnInit {
 
-  myForm: FormGroup = new FormGroup({
-    'name': new FormControl('RTX 4080ti')
+  // myForm: FormGroup = new FormGroup({
+  //   'name': new FormControl('RTX 4080ti')
+  // });
+
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['RTX 4080ti', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    quantity: [0, [Validators.required, Validators.min(0)]]
   });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
   }
