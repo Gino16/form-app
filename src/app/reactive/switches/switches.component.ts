49317f8c8ff6e9566c502bf52a46d7929fcc850c
Gino Ascencio <gino.francisco@hotmail.com>
2022-11-14 20:41:10 -0500

    add switch reactive forms

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.formBuilder.group({
+    gender: ['M', [Validators.required]],
+    notifications: [false, [Validators.required]],
+    terms: [false, [Validators.requiredTrue]]
+  });
+
+  person = {
+    gender: 'F',
+    notifications: true
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myForm.reset({ ...this.person, terms: true });
+    // this.myForm.get('terms')?.valueChanges.subscribe(term => console.log(term));
+    this.myForm.valueChanges.subscribe(form => {
+      delete form.terms;
+      this.person = form;
+    });
+  }
+
+  save() {
+    const formValue = { ...this.myForm.value };
+    delete formValue.terms;
+    this.person = formValue;
+    console.log(formValue);
   }
 
 }
